5940b43209d233bfbdf2e0f6fbaa4263553f163c
tsume-ha <50492756+tsume-ha@users.noreply.github.com>
2023-01-25 15:25:48 +0900

    ページ送りのテンプレートを作成した

--- near_search/views.py
+++ near_search/views.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from django.views.generic import TemplateView, ListView
 from django.core.exceptions import SuspiciousOperation
 from .forms import ShopSearchForm
@@ -10,6 +11,7 @@ class ShopListView(ListView):
     template_name = "near_search/shop_list.html"
     allow_empty = True
     model = None
+    search_queries = None
     paginate_by = 10
     total_shop_num = None
     shop_num = None
@@ -25,7 +27,8 @@ class ShopListView(ListView):
         shop_start_num = 1 + (int(float(page)) - 1) * 10 if int(float(page)) > 0 else 1
 
         hotpepper = API()
-        data = hotpepper.getShopList(form.cleaned_data, shop_start_num=shop_start_num)
+        self.search_queries = form.cleaned_data
+        data = hotpepper.getShopList(self.search_queries, shop_start_num=shop_start_num)
         self.total_shop_num = data["total_shop_num"]
         self.shop_num = data["shop_num"]
         self.shop_start_num = data["shop_start_num"]
@@ -45,3 +48,16 @@ class ShopListView(ListView):
             allow_empty_first_page=allow_empty_first_page,
             **kwargs,
         )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # ページ位置情報を保存しておく
+        paginator_urls = {
+            "first": "/search?" + urllib.parse.urlencode({**self.search_queries, **{"page": 1}}),
+            "prev": "/search?" + urllib.parse.urlencode({**self.search_queries, **{"page": context["page_obj"].previous_page_number() if context["page_obj"].has_previous() else 1 }}),
+            "current": "/search?" + urllib.parse.urlencode(self.search_queries),
+            "next": "/search?" + urllib.parse.urlencode({**self.search_queries, **{"page": context["page_obj"].next_page_number() if context["page_obj"].has_next() else context["page_obj"].paginator.num_pages}}),
+            "last": "/search?" + urllib.parse.urlencode({**self.search_queries, **{"page": context["page_obj"].paginator.num_pages}})
+        }
+        context["paginator_urls"] = paginator_urls
+        return context
